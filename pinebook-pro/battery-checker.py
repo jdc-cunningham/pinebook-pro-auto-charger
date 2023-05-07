64b8fb883411688ed4c5f207dd08b1e526866dff
Jacob Cunningham <jcun4128@gmail.com>
2023-05-07 10:39:33 -0500

    store battery capacity to variable

--- pinebook-pro/battery-checker.py
+++ pinebook-pro/battery-checker.py
@@ -1,10 +1,10 @@
-import os
+import subprocess
 import requests
 
 esp01_api = 'http://192.168.1.185'
 
 def get_battery_capacity():
-  return os.system('cat /sys/class/power_supply/cw2015-battery/capacity')
+  return subprocess.check_output('cat /sys/class/power_supply/cw2015-battery/capacity', shell=True)
 
 def turn_charger_off():
   requests.post(esp01_api, {'stop_charging': True})
@@ -13,11 +13,13 @@ def turn_charger_on():
   requests.post(esp01_api, {'start_charging': True})
 
 def check_battery():
-  battery_capacity = get_battery_capacity()
+  battery_capacity = int(get_battery_capacity())
 
   if (battery_capacity <= 80):
+    print('on')
     turn_charger_on()
   else:
+    print('off')
     turn_charger_off()
 
 check_battery()
